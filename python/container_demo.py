9a51e6f03c545f76a5008c58b953f341eefabf90
sapetnioc <sapetnioc@users.noreply.github.com>
2018-10-08 21:24:05 +0200

    Added thresold module and command using nibabel and numpy

---
+++ python/container_demo.py
@@ -0,0 +1,6 @@
+def threshold(image, threshold):
+    data = image.get_data()
+    idx_on = data[:,:,:] >= threshold
+    idx_off = data[:,:,:] < threshold
+    data[idx_on] = 1
+    data[idx_off] = 0
---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+
+from setuptools import setup, find_packages
+
+setup(
+    name='container_demo',
+    version='1.0',
+    description='Very simple python software used to illustrate Singularity or Docker usage',
+    packages=find_packages('python'),
+    scripts=['bin/cdemo_threshold'],
+    install_requires=[
+        'nibabel',
+        'numpy',
+    ]
+)
